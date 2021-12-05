af45af72c166a5e87eb48f141e39ef92bfdd54ab
michaelpanorios <michaelpanorios@gmail.com>
2021-12-05 19:29:42 +0200

    APVersion1.1: final version of back-end

--- src/adapter/DocumentParser.java
+++ src/adapter/DocumentParser.java
@@ -10,28 +10,31 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
 
 public class DocumentParser {
 
     public static void main(String[] args) {
         try {
-
             String home = System.getProperty("user.home");
             File xmlFile = new File(home+"/Downloads/" + "book-order.xml");
             System.out.println("XML input:");
+            /** Method which gets the XML file and setting the values to objects */
             showXMLfile(xmlFile);
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void showXMLfile(File xmlFile) throws ParserConfigurationException, IOException, SAXException {
+    public static void showXMLfile(File xmlFile) throws ParserConfigurationException, IOException, SAXException, ParseException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(xmlFile);
@@ -51,6 +54,70 @@ public class DocumentParser {
             elementsBookOrder.setPublish_date(element.getElementsByTagName("publish_date").item(0).getTextContent());
             elementsBookOrder.setDescription(element.getElementsByTagName("description").item(0).getTextContent());
             System.out.println(elementsBookOrder.toString());
+        }
+
+        /** User input validations start */
+        FieldsValidation validation = new FieldsValidation();
+        validation.populateValidations(elementsBookOrder);
+        /** Final XML output file */
+        showFinalXMLFile(elementsBookOrder);
+    }
+
+    private static void showFinalXMLFile(ElementsBookOrder elementsBookOrder) {
+        String home = System.getProperty("user.home");
+        final String path = home+"/Downloads/" + "book-order-final.xml";
+        try {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+
+            Document document = documentBuilder.newDocument();
+
+            /** Book element */
+            Element root = document.createElement("book");
+            document.appendChild(root);
+
+            /** Author element */
+            Element author = document.createElement("author");
+            author.appendChild(document.createTextNode(elementsBookOrder.getAuthor()));
+            root.appendChild(author);
+
+            /** Title element */
+            Element title = document.createElement("title");
+            title.appendChild(document.createTextNode(elementsBookOrder.getTitle()));
+            root.appendChild(title);
+
+            /** Genre element */
+            Element genre = document.createElement("genre");
+            genre.appendChild(document.createTextNode(elementsBookOrder.getGenre()));
+            root.appendChild(genre);
+
+            /** Price element */
+            Element price = document.createElement("price");
+            price.appendChild(document.createTextNode(elementsBookOrder.getPrice()));
+            root.appendChild(price);
+
+            /** Publish date element */
+            Element publish_date = document.createElement("publish_date");
+            publish_date.appendChild(document.createTextNode(elementsBookOrder.getPublish_date()));
+            root.appendChild(publish_date);
+
+            /** Description element */
+            Element description = document.createElement("description");
+            description.appendChild(document.createTextNode(elementsBookOrder.getDescription()));
+            root.appendChild(description);
+
+            /** Create the xml file and transform the DOM Object to an XML File */
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File(path));
+
+            transformer.transform(domSource, streamResult);
+
+
+        } catch (ParserConfigurationException | TransformerException pce) {
+            pce.printStackTrace();
         }
     }
 
--- src/adapter/FieldsValidation.java
+++ src/adapter/FieldsValidation.java
@@ -1,24 +1,47 @@
 package adapter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class FieldsValidation {
 
-    private void populateValidations(ElementsBookOrder elementsBookOrder){
+    String[] shopGenres = {"Action and Adventure", "Classic", "Novel", "Mystery", "Fantasy"};
+
+    public void populateValidations(ElementsBookOrder elementsBookOrder) throws ParseException {
         /** Name validation */
-        if(elementsBookOrder.getAuthor().isEmpty()) {
-            elementsBookOrder.setAuthor("ERROR");
-        } else {
-            elementsBookOrder.setAuthor(elementsBookOrder.getAuthor().toUpperCase());
-        }
+        if (elementsBookOrder.getAuthor().isEmpty()) elementsBookOrder.setAuthor("ERROR: Author field is blank.");
+        else elementsBookOrder.setAuthor(elementsBookOrder.getAuthor().toUpperCase());
+
         /** Title validation is not important */
+        elementsBookOrder.setTitle(elementsBookOrder.getTitle().trim());
 
         /** Genre validation within shop's available genres */
+        if (elementsBookOrder.getGenre().isEmpty()) elementsBookOrder.setGenre("ERROR: Genre field is blank.");
+        else checkExistingGenres(elementsBookOrder, elementsBookOrder.getGenre());
 
         /** Price validation for accepting only currency chars */
-
+        String regex = "(?:\\b(?:[BS]/\\.|R(?:D?\\$|p))|\\b(?:[TN]T|[CJZ])\\$|Дин\\.|\\b(?:Bs|Ft|Gs|K[Mč]|Lek|B[Zr]|k[nr]|[PQLSR]|лв|ден|RM|MT|lei|zł|USD|GBP|EUR|JPY|CHF|SEK|DKK|NOK|SGD|HKD|AUD|TWD|NZD|CNY|KRW|INR|CAD|VEF|EGP|THB|IDR|PKR|MYR|PHP|MXN|VND|CZK|HUF|PLN|TRY|ZAR|ILS|ARS|CLP|BRL|RUB|QAR|AED|COP|PEN|CNH|KWD|SAR)|\\$[Ub]|[\\p{Sc}ƒ])\\s?(?:\\d{1,3}(?:,\\d{3})*|\\d+)(?:\\.\\d{1,2})?(?!\\.?\\d)\n";
+        if (elementsBookOrder.getPrice().isEmpty() || !elementsBookOrder.getPrice().matches(regex)) {
+            elementsBookOrder.setPrice("ERROR: Price field is blank, it must start with currency character");
+        }
         /** Date validation which generates the date */
-
+            if (elementsBookOrder.getPublish_date().isEmpty()) {
+                elementsBookOrder.setPublish_date("ERROR");
+            } else {
+                Date date = new SimpleDateFormat("dd/MM/yyyy").parse(elementsBookOrder.getPublish_date());
+                elementsBookOrder.setPublish_date(String.valueOf("Order proceed on: " + java.util.Calendar.getInstance().getTime()));
+            }
         /** Description validation is not important */
+        elementsBookOrder.setDescription(elementsBookOrder.getDescription().trim());
 
     }
 
+    private void checkExistingGenres(ElementsBookOrder elementsBookOrder, String orderGenre) {
+        for (String shopGenre : shopGenres) {
+            if(shopGenre.equals(orderGenre)) elementsBookOrder.setGenre(orderGenre.toUpperCase());
+            else elementsBookOrder.setGenre("ERROR: Our shop do not provide this genre");
+        }
+    }
 }
+
